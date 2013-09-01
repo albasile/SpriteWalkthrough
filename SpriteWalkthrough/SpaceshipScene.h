ffe1de6b146738a2e0880077dd501fe9af6b2992
Antonio Basile <albasile2@gmail.com>
2013-09-01 17:40:49 -0300

    new changes

---
+++ SpriteWalkthrough/SpaceshipScene.h
@@ -0,0 +1,13 @@
+//
+//  SpaceshipScene.h
+//  SpriteWalkthrough
+//
+//  Created by Antonio Basile on 8/31/13.
+//  Copyright (c) 2013 com.bignerdranch. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SpaceshipScene : SKScene
+
+@end
